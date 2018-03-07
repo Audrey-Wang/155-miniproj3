005f69b3dabf7ce23f6508971d1a525f6e650cb3
Anne Zhou <annezhou@ugps-MacBook-6.local>
2018-03-07 15:44:35 -0800

    some stuff to start with

---
+++ proj3.py
@@ -0,0 +1,33 @@
+import os
+import numpy as np
+
+from HMM import unsupervised_HMM
+from HMM_helper import (
+    parse_observations,
+    sample_sentence,
+)
+
+# PREPROCESSING
+text = open(os.path.join(os.getcwd(), 'data/shakespeare.txt')).read()
+# TODO: extract words
+# - keep hyphenated words hyphenated
+# - separate punctuation from words, and store them separately
+obs, obs_map = parse_observations(text)
+# TODO: store syllable_dictionary.txt as a python dictionary for fast lookup
+# TODO: store syllable stress info from CMU for fast lookup
+
+# UNSUPERVISED LEARNING
+hmm8 = unsupervised_HMM(obs, 10, 100)
+
+# POETRY GENERATION, PART 1: HMMs
+# TODO: write poem generation using hmm.generate_emission()
+# - (suggested on piazza) in generate_emission() function, before generating 
+#   the next word, go through all possibilities and check for (1) whether 
+#   there are still enough syllables left for it and (2) whether it starts
+#   with the right stress. if one of those is violated, manually set its 
+#   probability to 0. renormalize and then pick a next-word.
+# - keep in mind last words may have special syllable counts and also 
+#   need to rhyme
+print('Sample Sentence:\n====================')
+print(sample_sentence(hmm8, obs_map, n_words=25))
+
