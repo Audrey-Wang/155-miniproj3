d3878690a68f8ff29556452aeaaf362f25bfb1f8
kavya9sreedhar <kavya9sreedhar@gmail.com>
2018-03-14 01:32:49 -0700

    added wordcloud code

--- proj3.py
+++ proj3.py
@@ -13,6 +13,8 @@ from helper import (
 # PREPROCESSING
 # text = open(os.path.join(os.getcwd(), 'data/shakespeare.txt')).read()
 text = open(os.path.join(os.getcwd(), 'data/allpoems.txt')).read()
+# visualization of whole data set
+wordcloud = text_to_wordcloud(text, title='Shakespeare')
 # TODO: extract words
 # - keep hyphenated words hyphenated
 # - some words could be tokenized as bigrams
@@ -23,7 +25,14 @@ for punct in [".", ",", ":", ";", "!", "?"]:
     syllables.update({punct:[[]]})
 
 # UNSUPERVISED LEARNING
-hmm8 = unsupervised_HMM(obs, 20, 100)
+#Was 20
+hmm8 = unsupervised_HMM(obs, 10, 100)
+
+# visualizations of sparsity of A, O as well as
+# visualizations of states as wordclouds
+visualize_sparsities(hmm8, O_max_cols=50)
+wordclouds = states_to_wordclouds(hmm8, obs_map)
+
 
 # POETRY GENERATION, PART 1: HMMs
 # TODO: write poem generation using hmm.generate_emission()
